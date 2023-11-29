e7697d36d7b2270971bbc91294c61097fdc99edc
hyunfunfun(gram) <hyunfunfun@naver.com>
2023-11-29 16:55:39 +0900

    Header

--- Final_TempleRun/Final_TempleRun/Header.h
+++ Final_TempleRun/Final_TempleRun/Header.h
@@ -3,15 +3,10 @@
 #include <GL/freeglut.h>
 #include <GL/freeglut_ext.h>
 
-//#include <gl/glm/glm.hpp>
-//#include <gl/glm/ext.hpp>
-//#include <gl/glm/gtc/matrix_transform.hpp>
+#include <gl/glm/glm.hpp>
+#include <gl/glm/ext.hpp>
+#include <gl/glm/gtc/matrix_transform.hpp>
 
-
-
-#include <gl\glm\glm\glm.hpp>
-#include <gl/glm/glm/ext.hpp>
-#include <gl/glm/glm/gtc/matrix_transform.hpp>
 
 #include <iostream>
 #include <stdlib.h>
